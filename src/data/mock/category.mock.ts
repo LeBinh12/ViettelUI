cb2fed2db32467cf64cd5ad20d5f97646a5c58fe
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-11-09 15:50:14 +0700

    add data cho table category để mock dữ liệu

---
+++ src/data/mock/category.mock.ts
@@ -0,0 +1,8 @@
+import type { Category } from "../../types/category";
+
+export const mockCategories: Category[] = [
+  { id: "cat-001", category_name: "Gói Data 5G" },
+  { id: "cat-002", category_name: "Gói Wifi / Internet" },
+  { id: "cat-003", category_name: "Gói Dịch Vụ Khác" },
+  { id: "cat-004", category_name: "Gói Combo Ưu Đãi" }, 
+];
