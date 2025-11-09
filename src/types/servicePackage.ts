ac2f5268ea52d2865c1f50e15287d8b26cf7e863
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-11-09 15:45:04 +0700

    add types servicePackage

---
+++ src/types/servicePackage.ts
@@ -0,0 +1,10 @@
+export interface ServicePackage {
+  id: string;
+  package_name: string;
+  price: number;
+  duration_months: number;
+  create_date: string;
+  update_date: string;
+
+  category_id?: string; 
+}
